18c75d4b7adf7dc2a8532501a20146351f507d36
Suzana Melo <suzanamelojor@gmail.com>
2022-01-27 21:27:32 +1300

    maps project: create custommap class

---
+++ CustomMap.ts
@@ -0,0 +1,14 @@
+export class CustomMap {
+  
+  googleMap: google.maps.Map;
+
+  constructor(divId: string) {
+    this.googleMap = new google.maps.Map(document.getElementById(divId), {
+      zoom: 1,
+      center: {
+        lat: 0,
+        lng: 0
+      }
+    })
+  }
+}
